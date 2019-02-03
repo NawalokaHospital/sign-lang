5d171ebdc530e1d9d3c33cf07d194f27d3315eb6
Praneeth Madusanka <wpraneethmadusanka@gmail.com>
2019-02-03 23:11:22 +0530

    add code for generated pickle

---
+++ 3test_images.py
@@ -0,0 +1,53 @@
+import tensorflow as tf
+import cv2
+import random
+import matplotlib.pyplot as plt
+import os
+import pickle
+import numpy as np
+
+DATADIR = 'test_images'
+CATEGORIES = os.listdir('test_images')
+IMG_SIZE = 50
+training_data = []
+x_train = []
+y_train = []
+num_of_class = 10
+
+
+def create_trainig_data():
+    for category in CATEGORIES:
+        path = os.path.join(DATADIR, category)
+        class_num = CATEGORIES.index(category)
+        for image in os.listdir(path):
+            try:
+                image_array = cv2.imread(os.path.join(path, image))
+                reiszed_image = cv2.resize(image_array, (IMG_SIZE, IMG_SIZE))
+                # plt.imshow(reiszed_image)
+                # plt.show()
+                training_data.append([reiszed_image, class_num])
+                # plt.imshow(reiszed_image)
+                # plt.show()
+            except Exception as e:
+                print("Exception.")
+
+
+create_trainig_data();
+random.shuffle(training_data)
+
+for image, label in training_data:
+    l_array = np.zeros(num_of_class)
+    l_array[label] = 1
+    x_train.append(image)
+    y_train.append(l_array)
+
+x_train = np.array(x_train).reshape(-1, IMG_SIZE, IMG_SIZE, 3)
+y_train = np.array(y_train)
+# save the training image set as pickle files
+pickle_out = open('test_images_pk', 'wb')
+pickle.dump(x_train, pickle_out)
+pickle_out.close()
+
+pickle_out = open('test_labels_pk', 'wb')
+pickle.dump(y_train, pickle_out)
+pickle_out.close()
